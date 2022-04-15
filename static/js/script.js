2340e5b03b319e044fea2e786f15937541c5ff96
Morpheus-23 <skyelah@icloud.com>
2022-04-15 10:58:24 +0000

    Fixed navbar and added logo navigation

--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,7 @@
-
 /*
     jQuery for MaterializeCSS initialization
 */
 
-$(document).ready(function () {
+$(document).ready(function(){
   $(".sidenav").sidenav({edge: "right"});
 });
-
-
-
-
-
-/*
-  vanilla JavaScript for MaterializeCSS initialization
-
-
-document.addEventListener('DOMContentLoaded', function () {
-     let sidenavs = document.querySelectorAll(".sidenav");
-     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
- });
-
- */
