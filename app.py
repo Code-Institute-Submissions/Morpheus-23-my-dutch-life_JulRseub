dd4fd572a7b84737dc84f824f07023ed478d642b
Morpheus-23 <skyelah@icloud.com>
2022-07-04 21:34:45 +0000

    minor amendments made to register page

--- app.py
+++ app.py
@@ -175,4 +175,4 @@ def delete_offer(offer_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
